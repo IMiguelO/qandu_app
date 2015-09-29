03451b3cf38364f8eac33394f4a906d2888f6708
Miguel <meolivaresa@gmail.com>
2015-09-29 18:33:33 +0000

    Add question update view

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns ('',
     url(r'^question/create/$', QuestionCreateView.as_view(), name='question_create'),
     url(r'^question/$', QuestionListView.as_view(), name='question_list'),
     url(r'^question/(?P<pk>\d+)/$', QuestionDetailView.as_view(), name='question_detail'),
+    url(r'^question/(?P<pk>\d+)/$', QuestionUpdateView.as_view(), name='question_update'),
 )
